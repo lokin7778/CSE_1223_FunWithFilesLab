2113e1dd8c20c37879976f92f5a82ab2dffe4fba
lokin7778 <lokin7778@gmail.com>
2025-11-09 18:19:54 -0500

    Copied the skeleton code for the program

---
+++ FunWithFiles.java
@@ -0,0 +1,36 @@
+/**
+ * Program to copy one file into another, using command line arguments
+ *
+ * @author Lokesh Narasimhan
+ * @author none
+ * @version 11/09/2025
+ *
+ */
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.Scanner;
+
+public class FunWithFiles {
+   
+   /**
+    * Displays the file given by fname to the screen
+    * 
+    * @param fname file to be displayed
+    */
+   public static void displayFile(String fname) {
+        //TODO: Open the file and print each line to the screen.
+        //TODO: Don't forget to close the file when you are done.
+        try {
+
+        } catch (FileNotFoundException e) {
+
+        }
+   }
+   
+
+    public static void main(String[] args) {
+      //TODO: Prompt the user for a filename        
+
+    }
+
+}
